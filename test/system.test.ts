89fa1199d7ecd591b479dd027c450013ec34a589
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-06-04 15:27:45 +0300

    Updating dependancies and getting the tests to run

--- test/system.test.ts
+++ test/system.test.ts
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import 'ts-node/register';
+import fetch from 'cross-fetch';
 import { expect } from 'chai';
 
 describe('Ocean Node Root Endpoint', () => {
   it('should return 200 OK', async () => {
-    const response = await axios.get('http://localhost:8000/');
+    const response = await fetch('http://localhost:8000/');
     console.log('response: ', response)
     expect(response.status).to.equal(200);
   });

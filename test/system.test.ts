dfe685e056fbf444504e0dffff5cb5872dec8d4f
Jamie Hewitt <jamie.hewitt15@gmail.com>
2024-06-04 19:52:14 +0300

    Adding more detailed test of the root endpooint

--- test/system.test.ts
+++ test/system.test.ts
@@ -5,7 +5,26 @@ import { expect } from 'chai';
 describe('Ocean Node Root Endpoint', () => {
   it('should return 200 OK', async () => {
     const response = await fetch('http://localhost:8000/');
-    console.log('response: ', response)
+    const responseBody = await response.json();
+
     expect(response.status).to.equal(200);
+    expect(responseBody).to.have.property('chainIds');
+    expect(responseBody).to.have.property('providerAddress');
+    expect(responseBody).to.have.property('serviceEndpoints');
+    expect(responseBody).to.have.property('software');
+    expect(responseBody).to.have.property('version');
+
+    // You can also check the values of the properties
+    expect(responseBody.software).to.equal('Ocean-Node');
+    
+    // Check if version is "0.0.1" or greater
+    const [major, minor, patch] = responseBody.version.split('.').map(Number);
+    expect(major).to.be.at.least(0);
+    if (major === 0) {
+      expect(minor).to.be.at.least(0);
+      if (minor === 0) {
+        expect(patch).to.be.at.least(1);
+      }
+    }
   });
 });

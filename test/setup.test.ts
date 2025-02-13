c099a3daf0c4270f700a68eccd7483df4526b65f
Jamie Hewitt <jamie.hewitt15@gmail.com>
2025-02-13 16:34:18 +0300

    Update tests

--- test/setup.test.ts
+++ test/setup.test.ts
@@ -5,26 +5,6 @@ import path from "path";
 describe("Ocean CLI Setup", function() {
     this.timeout(20000); // Set a longer timeout to allow the command to execute
 
-    it("should return an error message for 'npm run cli h' without MNEMONIC or PRIVATE_KEY", function(done) {
-        // Ensure the command is run from the project root directory
-        const projectRoot = path.resolve(__dirname, "..");
-
-        // Unset environment variables for the test
-        delete process.env.MNEMONIC;
-        delete process.env.PRIVATE_KEY;
-        delete process.env.RPC;
-
-        exec("npm run cli h", { cwd: projectRoot }, (error, stdout, stderr) => {
-            // Check the stderr for the expected error message
-            try {
-                expect(stderr).to.contain("Have you forgot to set MNEMONIC or PRIVATE_KEY?"); // Adjust this to match the expected output
-                done();
-            } catch (assertionError) {
-                done(assertionError);
-            }
-        });
-    });
-
     it("should return a valid response for 'npm run cli h' with MNEMONIC and PRIVATE_KEY", function(done) {
         // Ensure the command is run from the project root directory
         const projectRoot = path.resolve(__dirname, "..");

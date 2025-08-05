734ec5c1d4f00d512ad232ed56356ae173195505
giurgiur99 <giurgiur99@gmail.com>
2025-08-05 15:28:54 +0300

    wallet from mnemonic

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 import { Command } from 'commander';
 import { Commands } from './commands.js';
-import { JsonRpcProvider, Wallet, ethers } from 'ethers';
+import { JsonRpcProvider, Signer, ethers } from 'ethers';
 import chalk from 'chalk';
 import { stdin as input, stdout as output } from 'node:process';
 import { createInterface } from 'readline/promises';
@@ -10,17 +10,16 @@ import { toBoolean } from './helpers.js';
 async function initializeSigner() {
 
   const provider = new JsonRpcProvider(process.env.RPC);
-  let signer;
+  let signer: Signer;
 
   if (process.env.PRIVATE_KEY) {
     signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   } else {
-    signer = Wallet.fromPhrase(process.env.MNEMONIC);
-    signer = await signer.connect(provider);
+    signer = ethers.Wallet.fromPhrase(process.env.MNEMONIC, provider);
   }
 
   const { chainId } = await signer.provider.getNetwork();
-  return { signer, chainId };
+  return { signer, chainId: Number(chainId) };
 }
 
 export async function createCLI() {

aaab6d70708ffdcf8a738cd5122f70779cd86cb3
giurgiur99 <giurgiur99@gmail.com>
2025-08-05 14:40:36 +0300

    update libs and ethers

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 import { Command } from 'commander';
 import { Commands } from './commands.js';
-import { ethers } from 'ethers';
+import { JsonRpcProvider, Wallet, ethers } from 'ethers';
 import chalk from 'chalk';
 import { stdin as input, stdout as output } from 'node:process';
 import { createInterface } from 'readline/promises';
@@ -9,13 +9,13 @@ import { toBoolean } from './helpers.js';
 
 async function initializeSigner() {
 
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC);
+  const provider = new JsonRpcProvider(process.env.RPC);
   let signer;
 
   if (process.env.PRIVATE_KEY) {
     signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   } else {
-    signer = ethers.Wallet.fromMnemonic(process.env.MNEMONIC);
+    signer = Wallet.fromPhrase(process.env.MNEMONIC);
     signer = await signer.connect(provider);
   }
 
